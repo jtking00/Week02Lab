a7e40445f58f94456d599f6084b6540576eb59b4
jtking00 <122916900+jtking00@users.noreply.github.com>
2023-01-19 11:12:58 -0800

    Initialized the repo and created the base of hello world

---
+++ src/helloworld_week2lab/HelloWorld_Week2Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld_week2lab;
+
+/**
+ *
+ * @author 882199
+ */
+public class HelloWorld_Week2Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
