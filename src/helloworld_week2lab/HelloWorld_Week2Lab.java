23a04601f8161e1d4960595f8ce0d7c80b81f2fc
jtking00 <122916900+jtking00@users.noreply.github.com>
2023-01-19 11:14:42 -0800

    finished hello world

--- src/helloworld_week2lab/HelloWorld_Week2Lab.java
+++ src/helloworld_week2lab/HelloWorld_Week2Lab.java
@@ -15,7 +15,7 @@ public class HelloWorld_Week2Lab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
